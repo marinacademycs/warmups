aed22ecee2d6f8ea1002e525d0362265c80189cb
Kyle Vitale <53164742+KV-CS@users.noreply.github.com>
2022-01-19 09:06:49 -0800

    Update __init__.py

--- CS2/Unit0/warmup2/__init__.py
+++ CS2/Unit0/warmup2/__init__.py
@@ -1,6 +1,6 @@
 import check50
 
-#@check50.check()
-#def exists():
-#    """warmup2.py exists."""
-#    check50.exists("warmup2.py")
+@check50.check()
+def exists():
+    """warmup2.py exists."""
+    check50.exists("warmup2.py")
